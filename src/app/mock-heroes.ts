4b64ba246690dea7e3d96eebae565e5ddcf841ba
Shubham Mahajan <shubhammahajan1122@yahoo.com>
2018-08-08 17:53:48 +0530

    Changes for hero detail

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Strong'},
+    {id: 2, name: 'Narco'},
+    {id: 3, name: 'Bombardo'},
+    {id: 4, name: 'Adam'},
+    {id: 5, name: 'Michael'},
+    {id: 6, name: 'Phelps'},
+    {id: 7, name: 'Magneta'},
+    {id: 8, name: 'Riddle'},
+    {id: 9, name: 'Tornado'},
+    {id: 10, name: 'Clark'},
+    {id: 11, name: 'Hannibal'}
+];
